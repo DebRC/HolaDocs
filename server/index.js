11f6c3c1379dc0c49956c6160d90f5c9469b6dd3
DebRC <roychowdhurydebdoot@gmail.com>
2024-07-28 13:45:08 +0530

    Converted to Express App

--- server/index.js
+++ server/index.js
@@ -1,43 +1,16 @@
-const mongoose = require('mongoose')
-const Document = require('./models/document')
-
+const connectDB = require('./database');
+const socketConnection = require('./socket');
+const express = require('express');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log("MongoDB Connected…")
-    })
-    .catch(err => console.log(err))
+connectDB();
 
-const io = require('socket.io')(process.env.SERVER_PORT, {
-    cors: {
-        origin: process.env.CLIENT_URI,
-        methods: ["GET", "POST"]
-    }
-})
+const app = express();
+const server = require('http').createServer(app);
 
-io.on("connection", (socket) => {
-    socket.on("get-document", async (documentId) => {
-        const document = await findOrCreateDocument(documentId)
-        socket.join(documentId)
-        socket.emit("load-document", document.data)
+socketConnection(server);
 
-        socket.on("send-changes", (delta) => {
-            socket.broadcast.to(documentId).emit("receive-changes", delta)
-        })
-
-        socket.on("save-document", async (data) => {
-            await Document.findByIdAndUpdate(documentId, { data })
-        })
-    })
-})
-
-async function findOrCreateDocument(id){
-    if(id == null) return
-
-    const document = await Document.findById(id)
-
-    if(document) return document
-
-    return await Document.create({_id: id, data: process.env.DEFAULT_DOCUMENT})
-}
+const PORT = process.env.SERVER_PORT;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
